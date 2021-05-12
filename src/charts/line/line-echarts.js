dfc5e71518eb7ee74b3d82f6a39c3510abf81886
fen <fenfen.wff@dtwave-inc.com>
2021-05-12 18:55:16 +0800

    feat: 新增echart的折线图

--- src/charts/line/line-echarts.js
+++ src/charts/line/line-echarts.js
@@ -1,5 +1,9 @@
-import React from "react"
-import { LineChart, XAxis, YAxis, Tooltip, CartesianGrid, Line } from "recharts"
+/**
+ * 官方示例：https://echarts.apache.org/examples/zh/editor.html?c=line-smooth
+ */
+
+import * as echarts from 'echarts'
+import React from 'react'
 
 const data = [
   { name: "Page A", uv: 4000, pv: 2400, amt: 2400, time: 1 },
@@ -9,22 +13,36 @@ const data = [
   { name: "Page E", uv: 2500, pv: 4800, amt: 2181, time: 12 },
   { name: "Page F", uv: 1220, pv: 3800, amt: 2500, time: 16 },
   { name: "Page G", uv: 2300, pv: 4300, amt: 2100, time: 18 },
-  { name: "Page H", time: 24 },
 ]
 
-export const LineEcharts = (props) => (
-  <div>
-    <LineChart
-      width={400}
-      height={400}
-      data={data}
-      margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-    >
-      <XAxis dataKey="name" />
-      <Tooltip />
-      <CartesianGrid stroke="#f5f5f5" />
-      <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
-      <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
-    </LineChart>
-  </div>
-)
+export const LineEcharts = (props) => {
+  React.useEffect(() => {
+    var chartDom = document.getElementById('line-echarts')
+    var myChart = echarts.init(chartDom)
+    var option = {
+      xAxis: {
+        type: 'category',
+        data: data.map(item => item.name)
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        data: data.map(item => item.uv),
+        type: 'line',
+        smooth: true
+      },
+      {
+        data: data.map(item => item.pv),
+        type: 'line',
+        smooth: false
+      }],
+      tooltip: {
+        trigger: 'item' // 'axis'
+      },
+    }
+
+    option && myChart.setOption(option)
+  }, [])
+  return <div id="line-echarts" style={{height: 300, width: 400}}></div>
+}
